b153a50f43c6a8cd7528313e204732f698704dc0
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-08 18:04:11 -0400

    configured Async Image Fetch + UI Layout Cards

---
+++ MiChef/MiChef/Views/Componets/RecipeCard.swift
@@ -0,0 +1,43 @@
+//
+//  RecipeCard.swift
+//  MiChef
+//
+//  Created by Austin Potts on 8/8/23.
+//
+
+import SwiftUI
+
+struct RecipeCard: View {
+    var recipe: Recipe
+    
+    var body: some View {
+        
+        VStack {
+            //cannot add modifiers to aysnc image but on the image in the closure instead
+            AsyncImage(url: URL(string: recipe.image)) { image in
+                image
+                    .resizable()
+            } placeholder: {
+                Image(systemName: "photo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 40, height: 40, alignment: .center)
+                    .foregroundColor(.white.opacity(0.7))
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    
+         }
+            
+      }
+        .frame(width: 160, height: 217, alignment: .top)
+        .background(LinearGradient(gradient: Gradient(colors: [Color(.gray).opacity(0.3), Color(.gray)]), startPoint: .top, endPoint: .bottom))
+        .clipShape(RoundedRectangle(cornerRadius: 20))
+        .shadow(color: Color.black.opacity(0.3), radius: 15, x: 0, y: 10)
+
+    }
+}
+
+struct RecipeCard_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeCard(recipe: Recipe.all[0])
+    }
+}
