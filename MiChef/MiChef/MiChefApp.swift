c8c8edf470ff55302992d9597970faf8070b7c96
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-08 16:52:05 -0400

    First

---
+++ MiChef/MiChef/MiChefApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiChefApp.swift
+//  MiChef
+//
+//  Created by Austin Potts on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MiChefApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
