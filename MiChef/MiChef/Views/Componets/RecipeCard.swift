a872c90fc13a2d6f272509fae892613a9e630c0c
Austin Potts <austinpotts@Andariels-Air.lan>
2023-08-08 18:20:16 -0400

    configured LazyVGrid for Diaply of RecipeCards

--- MiChef/MiChef/Views/Componets/RecipeCard.swift
+++ MiChef/MiChef/Views/Componets/RecipeCard.swift
@@ -17,12 +17,22 @@ struct RecipeCard: View {
             AsyncImage(url: URL(string: recipe.image)) { image in
                 image
                     .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .overlay(alignment: .bottom) {
+                        Text(recipe.name)
+                            .font(.headline)
+                            .foregroundColor(.white)
+                            .shadow(color: .black, radius: 3, x: 0, y: 0)
+                            .frame(maxWidth: 136)
+                            .padding()
+                    }
             } placeholder: {
                 Image(systemName: "photo")
                     .resizable()
                     .scaledToFit()
                     .frame(width: 40, height: 40, alignment: .center)
                     .foregroundColor(.white.opacity(0.7))
+                    .shadow(color: .black, radius: 3, x: 0, y: 0)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     
          }
